893d9fe1e1a47917b7fcf4afc65b364a9c87eb04
fariasmk <fariasmk@hotmail.com>
2022-02-16 16:31:02 -0300

    Vers. 0.4

--- src/test/java/com/maikon/api/resources/UserResourceTest.java
+++ src/test/java/com/maikon/api/resources/UserResourceTest.java
@@ -27,8 +27,8 @@ class UserResourceTest {
 
     private static final Integer ID      = 1;
     private static final Integer INDEX   = 0;
-    private static final String NAME     = "Valdir";
-    private static final String EMAIL    = "valdir@mail.com";
+    private static final String NAME     = "Maikon";
+    private static final String EMAIL    = "maikon@email.com";
     private static final String PASSWORD = "123";
 
     private User user = new User();
